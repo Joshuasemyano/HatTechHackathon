4179cf4a74534759fa01a59bb7c3b753164e47d5
Joshuasemyano <b3academo.joshua@gmail.com>
2022-12-13 10:18:55 +0400

    Create MainFunc.py

---
+++ HatTech/MainFunc.py
@@ -0,0 +1,17 @@
+import array 
+
+import numpy as np
+from numpy.lib import recfunctions as nprf
+
+wlecMsg = np.array([0, "welcome msg", ""])
+langMenu = np.array([1, "languge menu", ""])
+MenuPersInfo = np.array([2, "personal information menu", ""])
+langInsure = np.array([3, "insurence menu", ""])
+catgMenu = np.array([4, "department menu", ""])
+durMenu = np.array([5, "duration menu", ""])
+issuDetlMenu = np.array([6, "issue details menu", ""])
+docMenu = np.array([7, "doctor list menu", ""])
+genrDocMenu = np.array([8, "general Doctors menu", ""])
+dateDayMenu = np.array([9, "date by day menu", ""])
+dateTimMenu = np.array([10, "date by time menu", ""])
+bayMSg = np.array([11, "bay message", ""])
