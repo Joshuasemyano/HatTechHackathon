4f69dfcdf66cd88ccdfc4d3c0fa3cce9483828c3
Joshuasemyano <b3academo.joshua@gmail.com>
2022-12-14 21:05:08 +0400

    Update MainFunc.py

--- HatTech/MainFunc.py
+++ HatTech/MainFunc.py
@@ -1,17 +1,41 @@
-import array 
+wlecMsg = [0, "welcome msg", "hi"]
+langMenu = [1, "languge menu", ""]
+MenuPersInfoEN = [2, "personal information menu", ""]
+langInsureEN = [3, "insurence menu", ""]
+catgMenuEN = [4, "department menu", ""]
+durMenuEN = [5, "duration menu", ""]
+issuDetlMenuEN = [6, "issue details menu", ""]
+docMenuEN = [7, "doctor list menu", ""]
+genrDocMenuEN = [8, "general Doctors menu", ""]
+dateDayMenuEN = [9, "date by day menu", ""]
+dateTimMenuEN = [10, "date by time menu", ""]
+bayMSgEN = [11, "bay message", ""]
 
-import numpy as np
-from numpy.lib import recfunctions as nprf
+MenuPersInfoAR = [2, "personal information menu",  "" ,  "Ar"]
+langInsureAR = [3, "insurence menu",   "",  "Ar"]
+catgMenuAR = [4, "department menu",   "",  "Ar"]
+durMenuAR = [5, "duration menu",   "",  "Ar"]
+issuDetlMenuAR = [6, "issue details menu", "",  "Ar"]
+docMenuAR = [7, "doctor list menu",   "",  "Ar"]
+genrDocMenuAR = [8, "general Doctors menu",   "",  "Ar"]
+dateDayMenuAR = [9, "date by day menu",   "",  "Ar"]
+dateTimMenuAR = [10, "date by time menu",   "",  "Ar"]
+bayMSgAR = [11, "bay message",  "" ,  "Ar"]
 
-wlecMsg = np.array([0, "welcome msg", ""])
-langMenu = np.array([1, "languge menu", ""])
-MenuPersInfo = np.array([2, "personal information menu", ""])
-langInsure = np.array([3, "insurence menu", ""])
-catgMenu = np.array([4, "department menu", ""])
-durMenu = np.array([5, "duration menu", ""])
-issuDetlMenu = np.array([6, "issue details menu", ""])
-docMenu = np.array([7, "doctor list menu", ""])
-genrDocMenu = np.array([8, "general Doctors menu", ""])
-dateDayMenu = np.array([9, "date by day menu", ""])
-dateTimMenu = np.array([10, "date by time menu", ""])
-bayMSg = np.array([11, "bay message", ""])
+
+def mainFun(menuNum):
+    if menuNum ==str(1):
+        print (wlecMsg[0])
+        print(wlecMsg[2])
+    elif menuNum == str(2):
+        print("Hello")
+      # print ("hihihihih")
+
+user_msg = input("Please insert a number: ")
+mainFun(user_msg)
+# print(wlecMsg)
+
+    
+
+
+#run. mainFun()
